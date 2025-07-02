bb1478983dcfa7ec07ff75354ecf0b529aa331f5
Sukhvinder Kaur <sukhvinderkaur2416@gmail.com>
2025-07-02 20:54:49 +0530

    routes added

---
+++ client/src/pages/educator/Dasboard.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Dasboard = () => {
+  return (
+    <div>
+      <h1>Educator Dashboard</h1>
+    </div>
+  )
+}
+
+export default Dasboard
---
+++ client/src/pages/student/CoursesList.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const CoursesList = () => {
+  return (
+    <div>
+      <h1>Course List Page</h1>
+    </div>
+  )
+}
+
+export default CoursesList
