3c52b29bd4c5683a0e5aa2c9b07a9227b9618fb6
Sukhvinder Kaur <sukhvinderkaur2416@gmail.com>
2025-08-15 09:54:35 +0530

    home section complete

--- client/src/components/student/Navbar.jsx
+++ client/src/components/student/Navbar.jsx
@@ -1,9 +1,12 @@
-import React, { use } from 'react'
+import React, { use, useContext } from 'react'
 import { assets } from '../../assets/assets'
 import { Link } from 'react-router-dom'
 import { useClerk, UserButton, useUser } from '@clerk/clerk-react'
+import { AppContext } from '../../context/AppContext'
 
 const Navbar = () => {
+
+  const {navigate, isEducator} = useContext(AppContext)
 
   const isCourseListPage = location.pathname.includes('/course-list');
   
@@ -11,13 +14,13 @@ const Navbar = () => {
   const {user} = useUser()
 
   return (
-    <div className={`flex items-center justify-between px-4 sm:px-10 md:px-14 lg:px-36 border-b border-gray-500 py-4 ${isCourseListPage ? 'bg-white' : 'bg-cyan-100/70'}`}>
-      <img src={assets.logo} alt="Logo" className='w-28 lg:w-32 cursor-pointer' />
+    <div className={`flex w-full items-center justify-between px-4 sm:px-10 md:px-14 lg:px-36 border-b border-gray-500 py-4 ${isCourseListPage ? 'bg-white' : 'bg-cyan-100/70'}`}>
+      <img onClick={()=> navigate('/')} src={assets.logo} alt="Logo" className='w-28 lg:w-32 cursor-pointer' />
       <div className='hidden md:flex items-center gap-5 text-gray-500'>
-        <div>
+        <div className='flex items-center gap-5'>
           { user && 
           <>
-            <button>Become Educator</button>
+            <button onClick={()=> {navigate('/educator')}}>{isEducator ? 'Educator Dashboard' : 'Become Educator'}</button>
           | <Link to='/my-enrollments'>My Enrollments</Link>
           </>
           }
@@ -30,7 +33,7 @@ const Navbar = () => {
         <div className='flex items-center gap-1 sm:gap-2 max-sm:text-xs'>
         { user && 
           <>
-            <button>Become Educator</button>
+            <button onClick={()=> {navigate('/educator')}}>{isEducator ? 'Educator Dashboard' : 'Become Educator'}</button>
           | <Link to='/my-enrollments'>My Enrollments</Link>
           </>
           }
