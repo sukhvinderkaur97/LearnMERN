5dfa733382f016b0ae8adaade974ec9eb708d18e
Sukhvinder Kaur <sukhvinderkaur2416@gmail.com>
2025-08-16 21:42:22 +0530

    frontent complete

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Route, Routes } from 'react-router-dom'
+import { Route, Routes, useMatch } from 'react-router-dom'
 import Home from './pages/student/Home'
 import CoursesList from './pages/student/CoursesList'
 import CourseDetails from './pages/student/CourseDetails'
@@ -12,11 +12,15 @@ import AddCourse from './pages/educator/AddCourse'
 import MyCourses from './pages/educator/MyCourses'
 import StudentsEnrolled from './pages/educator/StudentsEnrolled'
 import Navbar from './components/student/Navbar'
+import "quill/dist/quill.snow.css";
 
 const App = () => {
+
+  const isEducatorRoute = useMatch('/educator/*')
   return (
     <div className='text-default min-h-screen bg-white'>
-      <Navbar />
+      {!isEducatorRoute && <Navbar />}
+      
       <Routes>
         <Route path='/' element={<Home />}/>
         <Route path='/course-list' element={<CoursesList />}/>
