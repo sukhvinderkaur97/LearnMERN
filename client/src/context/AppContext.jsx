9a7d514ecaddd9b3b567a5bf7428998907ddb776
Sukhvinder Kaur <sukhvinderkaur2416@gmail.com>
2025-08-17 21:14:13 +0530

    stripe added

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -2,6 +2,7 @@ import { createContext, useEffect, useState } from "react";
 import { dummyCourses } from "../assets/assets";
 import { useNavigate } from "react-router-dom";
 import humanizeDuration from 'humanize-duration'
+import { useAuth, useUser } from "@clerk/clerk-react";
 
 export const AppContext = createContext()
 
@@ -9,6 +10,9 @@ export const AppContextProvider = (props) => {
 
     const currency = import.meta.env.VITE_CURRENCY
     const navigate = useNavigate()
+
+    const { getToken } = useAuth()
+    const { user } = useUser()
 
     const [allCourses, setAllCourses] = useState([])
     const [isEducator, setIsEducator] = useState(true)
@@ -65,7 +69,7 @@ export const AppContextProvider = (props) => {
     }
 
     // Fetch User Enrolled Courses
-    const fetchUserEnrolledCourses = async ()=>{
+    const fetchUserEnrolledCourses = async () => {
         setEnrolledCourses(dummyCourses)
     }
 
@@ -73,6 +77,16 @@ export const AppContextProvider = (props) => {
         fetchAllCourses()
         fetchUserEnrolledCourses()
     }, [])
+
+    const logToken = async ()=>{
+        console.log(await getToken());
+    }
+
+    useEffect(()=>{
+        if(user){
+            logToken()
+        }
+    },[user])
 
     const value = {
         currency, allCourses, navigate, calculateRating, isEducator, setIsEducator, calculateNoOfLectures, calculateCourseDuration, calculateChapterTime, enrolledCourses, fetchUserEnrolledCourses
--- server/controllers/webhooks.js
+++ server/controllers/webhooks.js
@@ -1,9 +1,12 @@
-import {Webhook} from "svix";
+import { Webhook } from "svix";
 import User from "../models/User.js";
+import Stripe from "stripe";
+import { Purchase } from "../models/Purchase.js";
+import Course from "../models/Course.js";
 
 // API Controller Function to manage Clerk User with database
 
-export const clerkWebhooks = async (req, res)=> {
+export const clerkWebhooks = async (req, res) => {
     try {
         const whook = new Webhook(process.env.CLERK_WEBHOOK_SECRET)
 
@@ -13,9 +16,9 @@ export const clerkWebhooks = async (req, res)=> {
             "svix-signature": req.headers["svix-signature"]
         })
 
-        const {data, type} = req.body
+        const { data, type } = req.body
 
-        switch(type) {
+        switch (type) {
             case 'user.created': {
                 const userData = {
                     _id: data.id,
@@ -39,7 +42,7 @@ export const clerkWebhooks = async (req, res)=> {
                 break;
             }
 
-            case 'user.deleted' : {
+            case 'user.deleted': {
                 await User.findByIdAndDelete(data.id)
                 res.json({})
                 break;
@@ -49,6 +52,71 @@ export const clerkWebhooks = async (req, res)=> {
                 break;
         }
     } catch (error) {
-        res.json({success: false, message: error.message})
+        res.json({ success: false, message: error.message })
     }
+}
+
+const stripeInstance = new Stripe(process.env.STRIPE_SECRET_KEY)
+
+export const stripeWebhooks = async (request, response) => {
+    const sig = request.headers['stripe-signature'];
+
+    let event;
+
+    try {
+        event = Stripe.webhooks.constructEvent(request.body, sig, process.env.STRIPE_WEBHOOK_SECRET);
+    }
+    catch (err) {
+        response.status(400).send(`Webhook Error: ${err.message}`);
+    }
+
+    // Handle the event
+    switch (event.type) {
+        case 'payment_intent.succeeded':{
+            const paymentIntent = event.data.object;
+            const paymentIntentId = paymentIntent.id;
+
+            const session = await stripeInstance.checkout.sessions.list({
+                payment_intent: paymentIntentId
+            })
+
+            const {purchaseId} = session.data[0].metadata;
+            const purchaseData = await Purchase.findById(purchaseId)
+            const userData = await User.findById(purchaseData.userId)
+            const courseData = await Course.findById(purchaseData.courseId.toString())
+
+            courseData.enrolledStudents.push(userData)
+            await courseData.save()
+
+            userData.enrolledCourses.push(courseData._id)
+            await userData.save()
+
+            purchaseData.status = 'completed'
+            await purchaseData.save()
+
+            break;
+        }
+
+        case 'payment_intent.payment_failed':{
+            const paymentIntent = event.data.object;
+            const paymentIntentId = paymentIntent.id;
+
+            const session = await stripeInstance.checkout.sessions.list({
+                payment_intent: paymentIntentId
+            })
+
+            const {purchaseId} = session.data[0].metadata;
+            const purchaseData = await Purchase.findById(purchaseId)
+            purchaseData.status = 'failed'
+            await purchaseData.save()
+
+            break;
+        }
+        // ... handle other event types
+        default:
+            console.log(`Unhandled event type ${event.type}`);
+    }
+
+    // Return a response to acknowledge receipt of the event
+    response.json({ received: true });
 }
